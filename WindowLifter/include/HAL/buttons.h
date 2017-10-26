2d9fd24503bad8e2ceba24dc33a3e31ca5e8df0a
luisarcpicazzo <luis.arc.picazzo@gmail.com>
2017-10-26 16:30:44 -0500

    cleaning Wspace

--- WindowLifter/include/HAL/buttons.h
+++
@@ -1,9 +0,0 @@
-#include "MAL/gpio.h"
-
-
-
-#define UP_Button PTC12
-#define DOWN_Button PTC13
-
-void config_Buttons_PortC(unsigned char num);
-unsigned char get_Button_Value(unsigned char num);
--- WindowLifter/include/clocks_and_modes.h
+++
@@ -1,5 +0,0 @@
-
-
-void SOSC_init_8MHz(void);
-void SPLL_init_160MHz(void);
-void NormalRUNmode_80MHz (void);
